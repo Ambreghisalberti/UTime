7d2246975014bb2b37e2fd2f4681611dd6582861
Ambre Ghisalberti <ambre.ghisalberti@lpp.polytechnique.fr>
2024-03-27 15:15:35 +0100

    Modified Windows class in UTime: prepare_df returns 3 outputs, not one.

--- UTime/__init__.py
+++ UTime/__init__.py
@@ -6,16 +6,17 @@ from swapp.windowing import make_windows
 class Windows(Dataset):
 
     def __init__(self, all_data, position, omni_data, win_length, label_paths, labelled_days):
-        self.df = make_windows.prepare_df(all_data, position, omni_data, win_length, label_paths, labelled_days)
+        self.df, self.pos, self.omni = make_windows.prepare_df(all_data, position, omni_data, win_length, label_paths, labelled_days)
         self.win_length = win_length
 
     def __getitem__(self, i):
         subdf = self.df.iloc[i:i + self.win_length]
         labels = subdf['label'].values
         subdf.drop(['label'], axis=1, inplace=True)
-        inputs = torch.tensor(subdf.values).double()
-        labels = torch.tensor(labels).double()
-        return subdf.index.values, inputs, labels
+        self.inputs = torch.tensor(subdf.values).double()
+        self.labels = torch.tensor(labels).double()
+        self.times = subdf.index.values
+        return self.times, self.inputs, self.labels
 
     def __len__(self):
         return len(self.df) // self.win_length
