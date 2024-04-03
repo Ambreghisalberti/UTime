6d60d822d7ee9ee7a7bd52e278674129de6bdb3e
Ambre Ghisalberti <ambre.ghisalberti@lpp.polytechnique.fr>
2024-04-03 14:54:23 +0200

    in Windows, replaced win_length by win_duration

--- UTime/__init__.py
+++ UTime/__init__.py
@@ -1,17 +1,17 @@
 import torch
 from torch.utils.data import Dataset
 from swapp.windowing.make_windows import prepare_df
-from swapp.windowing.make_windows.utils import select_windows
+from swapp.windowing.make_windows.utils import select_windows, durationToNbrPts, time_resolution
 import pandas as pd
 
 class Windows(Dataset):
 
-    def __init__(self, all_data, position, omni_data, win_length, label_paths, labelled_days):
-        self.df, self.pos, self.omni = prepare_df(all_data, position, omni_data, win_length, label_paths,
+    def __init__(self, all_data, position, omni_data, win_duration, label_paths, labelled_days):
+        self.df, self.pos, self.omni = prepare_df(all_data, position, omni_data, win_duration, label_paths,
                                                                labelled_days)
         self.dataset = pd.concat([self.df, self.omni], axis = 1)
         self.dataset = select_windows(self.dataset, ['isFull', 'isLabelled', 'encountersMSPandMSH'])
-        self.win_length = win_length
+        self.win_length = durationToNbrPts(win_duration, time_resolution(all_data))
 
     def __getitem__(self, i):
         subdf = self.df.iloc[i:i + self.win_length]
