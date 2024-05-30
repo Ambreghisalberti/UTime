cfc1b17f43d96ab85437f94798bf54282a5cc134
Ambre Ghisalberti <ambre.ghisalberti@lpp.polytechnique.fr>
2024-05-30 10:35:11 +0200

    Added parenthesis in label == True if (cond1 and cond2)

--- UTime/Training.py
+++ UTime/Training.py
@@ -137,14 +137,14 @@ class Training():
 
             while (self.current_epoch < self.epochs) & (early_stopping.early_stop == False):
                 #self.epoch(self.dltrain, fig=fig, ax=ax, **kwargs)
-                self.epoch(self.dltrain, label=True if label and self.current_epoch==0 else False, **kwargs)
+                self.epoch(self.dltrain, label=True if (label and self.current_epoch==0) else False, **kwargs)
                 early_stopping(self.val_loss[-1], self.model)
             self.stop_epoch = early_stopping.stop_epoch
 
         else:
             for e in range(self.epochs):
                 #self.epoch(self.dltrain, fig=fig, ax=ax, **kwargs)
-                self.epoch(self.dltrain, label=True if label and e==0 else False, **kwargs)
+                self.epoch(self.dltrain, label=True if (label and e==0) else False, **kwargs)
 
             self.stop_epoch = self.epochs
 
