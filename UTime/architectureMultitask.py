6c9464c2431b730c25082011603a11c81c1b1a5b
Ambre Ghisalberti <ambre.ghisalberti@lpp.polytechnique.fr>
2024-07-03 15:25:43 +0200

    Error in build_encoder2d du multitask

--- UTime/architectureMultitask.py
+++ UTime/architectureMultitask.py
@@ -32,7 +32,7 @@ class UTime(Architecture):
     def _build_encoder2D(self):
         layers = []
         for i in range(self.depth - 1):
-            layers = self._build_conv_block(i, self.nb_channels_spectro[i], self.kernels[i], self.sizes[i],
+            layers += self._build_conv_block(i, self.nb_channels_spectro[i], self.kernels[i], self.sizes[i],
                                            self.kernels[i])
             new_layers, pooling1, pooling2 = self.add_pooling(i, self.nb_channels_spectro[-1])
             layers += new_layers
